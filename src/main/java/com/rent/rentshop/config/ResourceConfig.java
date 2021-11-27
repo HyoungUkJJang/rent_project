10b20b2e31232e98eba0fea3dfe51442fded3306
HyoungUkJJang <guddnr1451@gmail.com>
2021-11-27 14:12:48 +0900

    이미지 파일 접근할 수 있도록 리소스 설정파일 생성

---
+++ src/main/java/com/rent/rentshop/config/ResourceConfig.java
@@ -0,0 +1,24 @@
+package com.rent.rentshop.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class ResourceConfig implements WebMvcConfigurer {
+
+    @Value(value = "${file.dir.product}")
+    private String fileDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/img/products/**")
+                .addResourceLocations("file:///" + fileDir)
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
+
+}
