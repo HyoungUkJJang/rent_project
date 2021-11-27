5d2593f72da1eeadc6a081c8e799698750bb6b6c
HyoungUkJJang <guddnr1451@gmail.com>
2021-11-27 14:16:25 +0900

    aws 설정파일 삭제

--- src/main/java/com/rent/rentshop/RentshopApplication.java
+++ src/main/java/com/rent/rentshop/RentshopApplication.java
@@ -9,14 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class RentshopApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location=" +
-			" classpath:application.yml," +
-			"classpath:aws.yml";
-
 	public static void main(String[] args) {
-//		new SpringApplicationBuilder(RentshopApplication.class)
-//				.properties(APPLICATION_LOCATIONS)
-//				.run(args);
 		SpringApplication.run(RentshopApplication.class, args);
 	}
 
