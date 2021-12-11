95e1c5fe46694739a93b6e71312e46ae7b99a5e6
HyoungUkJJang <guddnr1451@gmail.com>
2021-12-11 23:27:02 +0900

    유저 테스트코드 수정 트레비스 CI 설정파일 작성

--- src/test/java/com/rent/rentshop/member/domain/UserTest.java
+++ src/test/java/com/rent/rentshop/member/domain/UserTest.java
@@ -1,93 +1,24 @@
 package com.rent.rentshop.member.domain;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
-    private User user;
-    private PasswordEncoder passwordEncoder;
-
-    @BeforeEach
-    void prepare() {
-        user = User.builder()
-                .userEmail("mail@mail")
-                .userName("name1")
-                .userPhone("010")
-                .userBirth("1996")
+    @Test
+    public void 유저생성() {
+        User user = User.builder()
+                .email("mail@mail.com")
+                .password("12345")
+                .name("김형욱")
+                .phone("01012341234")
+                .birth("1996")
+                .bankName("국민은행")
+                .account("12341234")
                 .userAddress(new Address("road", "detail"))
                 .build();
-    }
 
-    @Test
-    @DisplayName("사용자 생성 테스트")
-    public void 사용자생성() throws Exception {
-
-        //GIVEN
-        String expectedUserEmail = "mail@mail";
-
-        //WHEN
-        String actualUserEmail = user.getUserEmail();
-
-        //THEN
-        assertEquals(expectedUserEmail,actualUserEmail);
-
-    }
-
-    @Test
-    @DisplayName("사용자 수정 테스트")
-    public void 사용자수정() throws Exception {
-
-        //GIVEN
-        String updateUserMail = "update@mail";
-        String updateUserPhone = "011";
-        String updateRoadAddress = "updateRoad";
-        String updateDetailAddress = "updateDetail";
-        user.updateUser(updateUserPhone,
-                updateRoadAddress, updateDetailAddress);
-
-        //WHEN
-        String resultUserMail = user.getUserEmail();
-
-        //THEN
-        assertEquals(updateUserMail, resultUserMail);
-
-    }
-
-    @Test
-    @DisplayName("패스워드 암호화 테스트")
-    public void 패스워드암호화() throws Exception {
-
-        //GIVEN
-        String rawPassword = "12345";
-        user.createPassword(rawPassword, passwordEncoder);
-
-        //WHEN
-        String secretPassword = user.getPassword();
-
-        //THEN
-        Assertions.assertThat(secretPassword.equals(rawPassword)).isFalse();
-
-    }
-
-    @Test
-    @DisplayName("패스워드 복호화 테스트")
-    public void 패스워드복호화() throws Exception {
-
-        //GIVEN
-        String rawPassword = "12345";
-
-        //WHEN
-        user.createPassword(rawPassword, passwordEncoder);
-
-        //THEN
-        Assertions.assertThat(user.passwordCheck(rawPassword, passwordEncoder)).isTrue();
-
+        Assertions.assertEquals("mail@mail.com", user.getEmail());
     }
 
 }
---
+++ src/test/java/com/rent/rentshop/member/dto/LoginDataTest.java
@@ -0,0 +1,14 @@
+package com.rent.rentshop.member.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginDataTest {
+
+    @Test
+    public void 로그인정보_생성() {
+
+    }
+
+}
