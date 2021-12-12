a4ebccf46386e9fbab6d5b9c7275c4663469fbfd
HyoungUkJJang <guddnr1451@gmail.com>
2021-12-13 01:16:10 +0900

    자동배포 쉘 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,5 +30,5 @@ echo "> Jar Name : $JAR_NAME"
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml \
   -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&q &
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 #  $REPOSITORY/$JAR_NAME 2>&1 &
