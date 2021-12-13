9a74e3fb604ad3e190d45ca6bc19e62691ad186a
HyoungUkJJang <guddnr1451@gmail.com>
2021-12-13 09:13:16 +0900

    인프라 세팅 완료
    * 소스코드 정리

--- src/main/java/com/rent/rentshop/non_rent/repository/NonUserRentRepository2.java
+++
@@ -1,8 +0,0 @@
-package com.rent.rentshop.non_rent.repository;
-
-import com.rent.rentshop.non_rent.domain.NonUserRent;
-import com.rent.rentshop.non_rent.domain.NonUserRent2;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NonUserRentRepository2 extends JpaRepository<NonUserRent2, Long> {
-}
