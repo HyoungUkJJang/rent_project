203b74c3cf58954f14cd92da7dda844fa5e61f9f
HyoungUkJJang <guddnr1451@gmail.com>
2021-12-14 16:13:43 +0900

    [ADD] - 상품서비스 테스트코드를 작성하자.
    * 조회, 상세조회, 등록, 삭제 테스트코드 작성

--- src/test/java/com/rent/rentshop/product/service/ProductServiceImplTest.java
+++ src/test/java/com/rent/rentshop/product/service/ProductServiceImplTest.java
@@ -1,56 +1,224 @@
 package com.rent.rentshop.product.service;
 
-import com.rent.rentshop.error.ProductNotFoundException;
 import com.rent.rentshop.member.domain.Address;
 import com.rent.rentshop.member.domain.User;
 import com.rent.rentshop.member.repository.UserRepository;
 import com.rent.rentshop.product.domain.Product;
-import com.rent.rentshop.product.dto.ProductUpdate;
 import com.rent.rentshop.product.repository.ProductRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-
+/*
+ * getProducts [v]
+ * getMyProducts []
+ * getProduct [v]
+ * register [v]
+ * update []
+ * delete [v]
+ */
 @DataJpaTest
 @DisplayName("ProductService 클래스")
 class ProductServiceImplTest {
 
-    private static final String USER_MAIL = "mail1@mail.com";
+    private String userEmail;
     private ProductService productService;
 
     @Autowired
     private ProductRepository productRepository;
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private Pageable pageable;
 
     @BeforeEach
     void setUp() {
+        User user = createUser();
+        User result = userRepository.save(user);
+        userEmail = result.getEmail();
         productService = new ProductServiceImpl(productRepository, userRepository);
     }
 
-//    @Nested
-//    @DisplayName("getProducts 메서드는")
-//    class Describe_getProducts {
-//
-//        @Nested
-//        @DisplayName("저장소에 상품들이 존재할 경우")
-//        class context_exist_products {
-//
-//            @BeforeEach
-//            void prepare() {
-//                for (int i = 0; i < 10; i++) {
-//
-//                }
-//            }
-//
-//        }
-//
-//    }
+    @AfterEach
+    void cleanUp() {
+        productRepository.deleteAll();
+    }
+
+    @Nested
+    @DisplayName("getProducts 메서드는")
+    class Describe_getProducts {
+
+        @Nested
+        @DisplayName("저장소에 상품들이 존재할 경우")
+        class Context_exist_products {
+
+            int size=5;
+            Slice<Product> products;
+
+            @BeforeEach
+            void prepare() {
+                for (int i = 1; i <= 10; i++) {
+                    Product product = Product.builder()
+                            .name("product" + i)
+                            .price(1000 * i)
+                            .deposit(5000 * i)
+                            .description("description" + i)
+                            .build();
+                    productService.register(product, userEmail);
+                }
+                Pageable pageable = PageRequest.of(0, size);
+                products = productService.getProducts(pageable);
+            }
+
+            @Test
+            @DisplayName("Slice 타입으로 상품목록을 리턴합니다.")
+            void It_return_products() {
+                assertThat(products).isNotEmpty();
+                assertEquals(5, products.getSize());
+            }
+        }
+
+        @Nested
+        @DisplayName("저장소에 상품들이 존재하지 않을 경우")
+        class Context_not_exist_products {
+
+            Slice<Product> products;
+
+            @BeforeEach
+            void prepare() {
+                productRepository.deleteAll();
+                Pageable pageable = PageRequest.of(0, 5);
+                products = productService.getProducts(pageable);
+            }
+
+            @Test
+            @DisplayName("비어있는 상품 리스트를 반환합니다.")
+            void It_return_empty_products() {
+                assertThat(products).isEmpty();
+            }
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("getProduct 메서드는")
+    class Describe_getProduct {
+
+        @Nested
+        @DisplayName("상품리스트 중 하나를 상세조회 하는 경우에")
+        class Context_products_detail {
+
+            Long productId;
+            String description = "description";
+
+            @BeforeEach
+            void prepare() {
+                Product product = productService.register(createProduct(), userEmail);
+                productId = product.getId();
+            }
+
+            @Test
+            @DisplayName("상품의 전체 정보를 조회하여 리턴합니다.")
+            void It_return_product_detail() {
+                Product findProduct = productService.getProduct(productId);
+                assertEquals(description, findProduct.getDescription());
+            }
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("register 메서드는")
+    class Describe_register {
+
+        @Nested
+        @DisplayName("등록할 상품이 있을 경우에")
+        class Context_exist_register_product {
+
+            Product product;
+            @BeforeEach
+            void prepare() {
+                product = Product.builder()
+                        .name("newProduct1")
+                        .price(2000)
+                        .deposit(25000)
+                        .description("newDescription")
+                        .build();
+            }
+
+            @Test
+            @DisplayName("저장소에 상품을 저장 후 리턴합니다.")
+            void It_saved_return_product() {
+                Product savedProduct = productService.register(product, userEmail);
+                assertEquals(product.getName(), savedProduct.getName());
+            }
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("delete 메서드는")
+    class Describe_delete {
+
+        @Nested
+        @DisplayName("자신이 올린 상품들 중 삭제하고 싶은 상품이 있을 경우에")
+        class Context_exist_delete_product {
+
+            Long productId;
+            @BeforeEach
+            void prepare() {
+                productRepository.deleteAll();
+                Product registerProduct = productService.register(createProduct(), userEmail);
+                productId = registerProduct.getId();
+            }
+
+            @Test
+            @DisplayName("상품의 아이디를 통해 상품을 삭제합니다.")
+            void It_delete_product() {
+                productService.delete(productId);
+                Slice<Product> products = productService.getProducts(PageRequest.of(0, 5));
+                assertThat(products).isEmpty();
+            }
+
+        }
+
+    }
+
+    /**
+     * 테스트 상품을 생성합니다.
+     * @return 테스트 상품 객체
+     */
+    private Product createProduct() {
+        return Product.builder()
+                .name("productName1")
+                .price(1000)
+                .deposit(15000)
+                .description("description")
+                .build();
+    }
+
+    /**
+     * 테스트 유저를 생성합니다.
+     * @return 테스트 유저 객체
+     */
+    private User createUser() {
+        return User.builder()
+                .email("mail@mail.com")
+                .password("12345")
+                .name("hyounguk")
+                .phone("010")
+                .birth("1996")
+                .bankName("kakaoBank")
+                .account("123-123")
+                .userAddress(new Address("road", "detail"))
+                .build();
+    }
+
 }
