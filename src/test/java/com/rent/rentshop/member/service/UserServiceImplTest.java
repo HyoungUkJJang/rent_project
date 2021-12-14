6d792c6292d69df8dfde13ae66b381825613204d
HyoungUkJJang <guddnr1451@gmail.com>
2021-12-14 15:56:04 +0900

    [ADD] - 상품서비스 테스트코드를 작성하자.
    * 조회, 상세조회, 등록, 삭제 테스트코드 작성

--- src/test/java/com/rent/rentshop/member/service/UserServiceImplTest.java
+++ src/test/java/com/rent/rentshop/member/service/UserServiceImplTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,5 +23,60 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @DisplayName("UserService 클래스")
 class UserServiceImplTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserServiceImpl(userRepository);
+    }
+
+    @Nested
+    @DisplayName("join 메소드는")
+    class Describe_join {
+
+        @Nested
+        @DisplayName("가입할 유저가 있을 경우에")
+        class Context_exist_user {
+
+            String email;
+            User result;
+            @BeforeEach
+            void prepare() {
+                User user = createUser();
+                result = userService.join(user);
+                email = result.getEmail();
+            }
+
+            @Test
+            @DisplayName("사용자 정보를 저장소에 저장 후 리턴합니다.")
+            void It_return_save_user() {
+                String findEmail = result.getEmail();
+                assertEquals(email, findEmail);
+            }
+        }
+
+    }
+
+    /**
+     * 테스트 유저를 생성합니다.
+     * @return 테스트 유저 객체
+     */
+    private User createUser() {
+        return User.builder()
+                .email("mail@mail.com")
+                .password("12345")
+                .name("hyounguk")
+                .phone("010")
+                .birth("1996")
+                .bankName("kakaoBank")
+                .account("123-123")
+                .userAddress(new Address("road", "detail"))
+                .build();
+    }
 
 }
