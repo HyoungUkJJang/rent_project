a196d3cfc5135c34791200c42b90d79974b6e78a
HyoungUkJJang <guddnr1451@gmail.com>
2021-12-14 10:35:13 +0900

    상품 서비스 테스트코드 초안 작성

--- src/test/java/com/rent/rentshop/product/service/ProductServiceImplTest.java
+++ src/test/java/com/rent/rentshop/product/service/ProductServiceImplTest.java
@@ -10,21 +10,47 @@ import com.rent.rentshop.product.repository.ProductRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Pageable;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("ProductService 클래스")
+
 @DataJpaTest
+@DisplayName("ProductService 클래스")
 class ProductServiceImplTest {
 
-    private static final String USER_MAIL = "userMail1";
+    private static final String USER_MAIL = "mail1@mail.com";
     private ProductService productService;
 
     @Autowired
     private ProductRepository productRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private Pageable pageable;
 
+    @BeforeEach
+    void setUp() {
+        productService = new ProductServiceImpl(productRepository, userRepository);
+    }
 
+//    @Nested
+//    @DisplayName("getProducts 메서드는")
+//    class Describe_getProducts {
+//
+//        @Nested
+//        @DisplayName("저장소에 상품들이 존재할 경우")
+//        class context_exist_products {
+//
+//            @BeforeEach
+//            void prepare() {
+//                for (int i = 0; i < 10; i++) {
+//
+//                }
+//            }
+//
+//        }
+//
+//    }
 }
