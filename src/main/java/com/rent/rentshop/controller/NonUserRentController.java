7d69eac67ddbc485fff616d77d0009429cf3ba98
HyoungUkJJang <guddnr1451@gmail.com>
2021-12-17 18:08:35 +0900

    비회원 상품대여 신청 경로수정

--- src/main/java/com/rent/rentshop/controller/NonUserRentController.java
+++ src/main/java/com/rent/rentshop/controller/NonUserRentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("rent/nonuser/borrow")
+@RequestMapping("/rent/nonuser/borrow")
 @RequiredArgsConstructor
 @CrossOrigin
 public class NonUserRentController {
